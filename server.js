295d58b57a41a122f55fa770bc8ae8bf1065c29f
Arnaud DJIM-ASSEL RIBAR <arnaud.djim.assel.ribar@gmail.com>
2025-10-07 15:31:54 +0100

    add others endpoints

--- server.js
+++ server.js
@@ -16,6 +16,21 @@ app.get('/items', (req, res) => {
   res.status(200).json({items : items});
 });
 
+app.get('/items/:id', (req, res) => {
+  listResult = items.filter(item => item.id === parseInt(req.params.id));
+  if(listResult.length === 0){
+    return res.status(404).json({message : 'Item not found'});
+  }
+
+  res.status(200).json(listResult[0]);
+});
+
+app.delete('/items/:id', (req, res) => {
+    listResult = items.filter(item => item.id !== parseInt(req.params.id));
+
+    res.status(200).send(listResult);
+});
+
 
 app.post('/items', (req, res) => {
   const item = req.body;
@@ -25,6 +40,17 @@ app.post('/items', (req, res) => {
   }else{
     res.status(400).json({message: 'Item is required'});
   }
+});
+
+app.put('/items/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = items.findIndex(item => item.id === id);
+    if (index !== -1) {
+        items[index] = { ...items[index], ...req.body };
+        res.status(200).send(items[index]);
+    } else {
+        res.status(404).send({ message: 'Item not found' });
+    }
 });
 
 //app.get('*', handler.errorHandler); 
