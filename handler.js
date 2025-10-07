cfdd967c85b2dad6a3fbcbdb9f5447f2ce3d60bd
Arnaud DJIM-ASSEL RIBAR <arnaud.djim.assel.ribar@gmail.com>
2025-10-07 16:01:50 +0100

    Add handlers

--- handler.js
+++ handler.js
@@ -1,14 +1,47 @@
 
 const blogs = {results : []}
 
-module.exports.getHandler = (req,res)=>{
-
+module.exports.getHandler = (req,res, items)=>{
+      res.status(200).json({items : items});
 }
-module.exports.postHandler = (req,res)=>{
 
+module.exports.postHandler = (req,res,items)=>{
+      const item = req.body;
+      if (item) {
+            items.push(item);
+            res.status(201).json({message: 'Item added', item: item});
+      }else{
+            res.status(400).json({message: 'Item is required'});
+      }
+ }
+
+ module.exports.getByIdHandler = (req,res,items)=>{
+      listResult = items.filter(item => item.id === parseInt(req.params.id));
+      if(listResult.length === 0){
+            return res.status(404).json({message : 'Item not found'});
+      }
+      res.status(200).json(listResult[0]);
+ }
+
+ module.exports.putHandler = (req,res,items)=>{
+      const id = parseInt(req.params.id);
+      const index = items.findIndex(item => item.id === id);
+      if (index !== -1) {
+            items[index] = { ...items[index], ...req.body };
+            res.status(200).send(items[index]);
+      } else {
+            res.status(404).send({ message: 'Item not found' });
+      }
+ }
+
+module.exports.deleteHandler = (req,res,items)=>{
+    listResult = items.filter(item => item.id !== parseInt(req.params.id));
+
+    res.status(200).send(listResult);
 }
+
 module.exports.errorHandler = (req,res)=>{
-      res.status(404).send({ error: 'Not Found Arnaud Ribar' });
+      res.status(404).send({ error: 'Not Found' });
 }
 
 module.exports.mainHandler = (req,res)=>{
--- server.js
+++ server.js
@@ -13,33 +13,20 @@ app.use(express.json())
 app.get('/', handler.mainHandler)
 
 app.get('/items', (req, res) => {
-  res.status(200).json({items : items});
+  handler.getHandler(req, res, items);
 });
 
 app.get('/items/:id', (req, res) => {
-  listResult = items.filter(item => item.id === parseInt(req.params.id));
-  if(listResult.length === 0){
-    return res.status(404).json({message : 'Item not found'});
-  }
-
-  res.status(200).json(listResult[0]);
+  handler.getByIdHandler(req, res, items);
 });
 
 app.delete('/items/:id', (req, res) => {
-    listResult = items.filter(item => item.id !== parseInt(req.params.id));
-
-    res.status(200).send(listResult);
+    handler.deleteHandler(req, res, items);
 });
 
 
 app.post('/items', (req, res) => {
-  const item = req.body;
-  if (item) {
-    items.push(item);
-    res.status(201).json({message: 'Item added', item: item});
-  }else{
-    res.status(400).json({message: 'Item is required'});
-  }
+  handler.postHandler(req, res, items);
 });
 
 app.put('/items/:id', (req, res) => {
@@ -53,9 +40,9 @@ app.put('/items/:id', (req, res) => {
     }
 });
 
-//app.get('*', handler.errorHandler); 
 //error handler
 app.use(handler.errorHandler); // Global error handler
+
 app.listen(port, () => {
   console.log(`Express app listening on port http://localhost:${port}`)
 })
